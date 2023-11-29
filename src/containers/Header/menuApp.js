ca33d6c83822b0b0a5bdc1b50f5f343908112710
DESKTOP-CHB69CQ\Nguyen Trung Tuyen <trungtuyenbii@gmail.com>
2023-11-29 21:16:25 +0700

    no message

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -11,9 +11,13 @@ export const adminMenu = [
             {
                 name: 'menu.admin.manage-doctor', link: '/system/manage-doctor'
             },
-            {
-                name: 'menu.admin.manage-admin', link: '/system/user-admin'
-            },
+            //  {
+            //      name: 'menu.admin.manage-admin', link: '/system/user-admin'
+            // },
+     { // Quản lý kế hoạch khám bệnh của bác sĩ
+       
+             name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'     
+    },  
         ]
     },
     { // Quản lý phòng khám
@@ -41,4 +45,15 @@ export const adminMenu = [
         ]
     }
 
+];
+
+export const doctorMenu = [
+    {
+    name: 'menu.admin.manage-user', 
+    menus:[
+    { // Quản lý kế hoạch khám bệnh của bác sĩ
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+    },
+]
+}
 ];
